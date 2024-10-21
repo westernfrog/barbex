3f7963bc34177753f2f24f31ffa963bbde040feb
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2024-10-21 09:57:41 +0530

    Updated

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,22 @@
-import { ChevronsRightIcon, PhoneIcon, SearchIcon } from "lucide-react";
+"use client";
+
+import {
+  ChevronsRightIcon,
+  MenuIcon,
+  PhoneIcon,
+  SearchIcon,
+  XIcon,
+} from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -10,10 +27,12 @@ const navigation = [
 ];
 
 export default function Header(params) {
+  const pathname = usePathname();
+  const [open, setOpen] = useState(false);
   return (
     <>
       <header className="sticky z-50 top-0 inset-x-0 bg-dark text-white">
-        <div className="flex items-center justify-between lg:px-16 px-6 py-6">
+        <div className="flex items-center justify-between lg:px-16 px-6 lg:py-6 py-4">
           <div className="flex items-center divide-x divide-white">
             <Link
               href="/"
@@ -58,6 +77,76 @@ export default function Header(params) {
               </li>
             </ul>
           </nav>
+          <button onClick={() => setOpen(true)}>
+            <MenuIcon size={26} />
+          </button>
+          <Transition show={open}>
+            <Dialog className="relative z-50" onClose={setOpen}>
+              <TransitionChild
+                enter="ease-in-out duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-500"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-gray bg-opacity-75 transition-opacity" />
+              </TransitionChild>
+              <div className="fixed inset-0 overflow-hidden">
+                <div className="absolute inset-0 overflow-hidden">
+                  <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full">
+                    <TransitionChild
+                      enter="transform transition ease-in-out duration-500 sm:duration-700"
+                      enterFrom="translate-x-full"
+                      enterTo="translate-x-0"
+                      leave="transform transition ease-in-out duration-500 sm:duration-700"
+                      leaveFrom="translate-x-0"
+                      leaveTo="translate-x-full"
+                    >
+                      <DialogPanel className="pointer-events-auto relative w-screen max-w-md">
+                        <div className="flex h-full flex-col overflow-y-scroll bg-dark text-white p-6 shadow-xl">
+                          <div className="flex justify-end">
+                            <button className="" onClick={() => setOpen(false)}>
+                              <XIcon size={28} strokeWidth={1.5} />
+                            </button>
+                          </div>
+                          <nav className="relative mt-6 flex-1 divide-y divide-dark/60">
+                            {navigation.map((item, index) => (
+                              <div key={index}>
+                                <Link
+                                  href={item.href}
+                                  onClick={() =>
+                                    setTimeout(() => setOpen(false), 500)
+                                  }
+                                  className="block py-4 border-b-2 border-dashed border-white text-xl"
+                                >
+                                  {item.name}
+                                </Link>
+                              </div>
+                            ))}
+                            <div className="flex items-center gap-2 py-10">
+                              <div className="ring-1 ring-white p-2 rounded-full">
+                                <PhoneIcon size={14} />
+                              </div>
+                              <Link
+                                href="tel:+125 (895) 658"
+                                onClick={() =>
+                                  setTimeout(() => setOpen(false), 500)
+                                }
+                                className="block text-lg"
+                              >
+                                Hotline +125 (895) 658
+                              </Link>
+                            </div>
+                          </nav>
+                        </div>
+                      </DialogPanel>
+                    </TransitionChild>
+                  </div>
+                </div>
+              </div>
+            </Dialog>
+          </Transition>
         </div>
       </header>
     </>
